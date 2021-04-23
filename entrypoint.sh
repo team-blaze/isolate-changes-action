9d86e5f83b155f1829093938dbf26018a468d19f
Craig Mulligan <hey@hobochild.com>
2021-04-23 10:13:02 +0200

    fixup

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,11 @@ echo "Isolate Folder: $isolate_folder";
 echo "From: $from_ref";
 
 # grep will exit with a 1 if no matches are found.
-git diff --name-only origin/$from_ref^.. | grep -s -q "^${isolate_folder}*"
+git diff --name-only origin/$from_ref.. | grep -s -q ^${isolate_folder}*
 has_isolated_folder_changes=$?
 
 # grep -v inverts the search.
-git diff --name-only origin/$from_ref^.. | grep -s -v -q ^$isolate_folder*
+git diff --name-only origin/$from_ref.. | grep -s -v -q ^$isolate_folder*
 has_other_changes=$?
 
 # add exit codes - should only be zero if matches were
